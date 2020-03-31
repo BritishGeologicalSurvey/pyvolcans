8d0a8e55530e32029cda7b3d3c309be0dee41350
PTierz <pablo@bgs.ac.uk>
2020-03-31 13:11:00 +0100

    Add argument parser for user-defined inputs

--- pyvolcans.py
+++ pyvolcans.py
@@ -10,6 +10,8 @@ Created on Mon Feb 17 12:13:20 2020
 
 from pymatreader import read_mat
 from pathlib import Path
+import argparse
+
 
 ANALOGY_DIR = Path("VOLCANS_mat_files/analogy_mats")
 
@@ -23,3 +25,17 @@ eruption_size_analogy = read_mat(ANALOGY_DIR /
                                  "ASzfinal_allvolcs_SINA.mat")['ASz_allcross']
 eruption_style_analogy = read_mat(ANALOGY_DIR / 
                                   "AStfinal_allvolcs_SINA.mat")['ASt_allcross']
+
+#var1 = print('Select the weight for tectonic setting')
+#var2
+#...
+#var5
+#
+#weights = {var1,...,var5}
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tectonic_setting", help="Set tectonic setting, default value=0.2")
+       
+    args = parser.parse_args()
+    tectonic_setting = args.tectonic_setting 
+    calculated_weighted_analogy_matrix(tectonic_setting)
