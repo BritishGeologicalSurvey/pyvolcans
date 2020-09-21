846d7664c30e1f70557553bb01ac3739fe8a05c0
John A Stevenson <jostev@bgs.ac.uk>
2020-09-21 17:23:48 +0100

    Fix pylint errors

--- test/test_pyvolcans.py
+++ test/test_pyvolcans.py
@@ -4,7 +4,6 @@ Created on Fri May 15 12:49:55 2020
 
 @author: pablo
 """
-
 from pyvolcans.pyvolcans_func import (
     fuzzy_matching,
     get_volcano_idx_from_name,
@@ -12,15 +11,17 @@ from pyvolcans.pyvolcans_func import (
     get_volcano_number_from_name
 )
 
+# pylint: disable=missing-docstring
+
 
 def test_volcano_idx():
     idx = get_volcano_idx_from_name(volcano_name='Fuego')
-    assert 1071 == idx
+    assert idx == 1071
 
 
 def test_volcano_name():
     name = get_volcano_name_from_idx(volcano_idx=1071)
-    assert 'Fuego' == name
+    assert name == 'Fuego'
 
 
 def test_fuzzy_matching():
@@ -29,6 +30,7 @@ def test_fuzzy_matching():
     name = fuzzy_matching('West Eiffel')
     assert isinstance(name, str)
 
+
 def test_volcano_number():
     number = get_volcano_number_from_name('Santorini')
-    assert 212040 == number
+    assert number == 212040
