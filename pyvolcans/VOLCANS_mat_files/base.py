13384af65644b3e1d27441ef3d75fd3b77cf387c
mobiuscreek <vyronc@bgs.ac.uk>
2020-10-15 12:48:38 +0100

    Add functions for loading mat files

---
+++ pyvolcans/VOLCANS_mat_files/base.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pathlib import Path
+from pymatreader import read_mat
+
+BASE_DIR = Path(__file__).resolve().parent
+DIRNAME_ANALOGY= BASE_DIR.joinpath("analogy_mats")
+DIRNAME_VOLCANO= BASE_DIR.joinpath("VOTW_prepared_data")
+
+
+def load_tectonic_analogy():
+    return read_mat(DIRNAME_ANALOGY.joinpath("ATfinal_allvolcs.mat"))["AT_allcross"]
+
+def load_geochemistry_analogy():
+    return read_mat(DIRNAME_ANALOGY.joinpath("AGfinal_allvolcs_ALU.mat"))["AG_allcross"]
+
+def load_morphology_analogy():
+    return read_mat(DIRNAME_ANALOGY.joinpath("AMfinal_allvolcs.mat"))["AM_allcross"]
+
+def load_eruption_size_analogy():
+    return read_mat(DIRNAME_ANALOGY.joinpath("ASzfinal_allvolcs_SINA.mat"))["ASz_allcross"]
+
+def load_eruption_style_analogy():
+    return read_mat(DIRNAME_ANALOGY.joinpath("AStfinal_allvolcs_SINA.mat"))["ASt_allcross"]
+
+def load_volcano_names():
+    return pd.read_csv(DIRNAME_VOLCANO.joinpath("volc_names.csv"), header = None)
+
