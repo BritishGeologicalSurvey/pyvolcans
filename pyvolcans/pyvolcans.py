f8d0a9645e094010d2d06ea0f1af9c52544b7197
PTierz <pablo@bgs.ac.uk>
2020-11-13 11:56:44 +0000

    Accept Smithsonian ID as argument

--- pyvolcans/pyvolcans.py
+++ pyvolcans/pyvolcans.py
@@ -20,6 +20,7 @@ from pymatreader import read_mat
 #personal packages
 from pyvolcans.pyvolcans_func import (
     _frac_to_float,
+    get_volcano_name_from_volcano_number,
     calculate_weighted_analogy_matrix,
     get_analogies,
     get_many_analogy_percentiles,
@@ -32,34 +33,63 @@ handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logging.basicConfig(handlers=[handler], level=logging.INFO)
 
-#loading the files required to calculate analogies and analogue volcanoes
-#ANALOGY_DIR = Path("VOLCANS_mat_files/analogy_mats")
-#
-#tectonic_analogy = read_mat(ANALOGY_DIR / 
-#                        "ATfinal_allvolcs.mat")['AT_allcross']
-#geochemistry_analogy = read_mat(ANALOGY_DIR / 
-#                            "AGfinal_allvolcs_ALU.mat")['AG_allcross']
-#morphology_analogy = read_mat(ANALOGY_DIR / 
-#                          "AMfinal_allvolcs.mat")['AM_allcross']
-#eruption_size_analogy = read_mat(ANALOGY_DIR / 
-#                             "ASzfinal_allvolcs_SINA.mat")['ASz_allcross']
-#eruption_style_analogy = read_mat(ANALOGY_DIR / 
-#                              "AStfinal_allvolcs_SINA.mat")['ASt_allcross']
-
-#var1 = print('Select the weight for tectonic setting')
-#var2
-#...
-#var5
-#
-#weights = {var1,...,var5}
-    
 def cli():
     """
     Command line interface to run Pyvolcans and return analogues.
     """
+    args = parse_args()
+    
+    #get volcano_name from volcano arg
+    try:
+        volcano_id = int(args.volcano)
+        volcano_name = get_volcano_name_from_volcano_number(volcano_id)
+    except ValueError:
+        volcano_name = args.volcano
+    except PyvolcansError as exc:
+        # Print error message and quit program on error
+        logging.error(exc.args[0])
+        sys.exit(1)
+    
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    #defining some intermediate variables
+    count = args.count
+    arg_weights = {'tectonic_setting': _frac_to_float(args.tectonic_setting),
+               'geochemistry': _frac_to_float(args.rock_geochemistry),
+               'morphology': _frac_to_float(args.morphology),
+               'eruption_size': _frac_to_float(args.eruption_size),
+               'eruption_style': _frac_to_float(args.eruption_style)}
+
+    my_apriori_volcanoes = args.apriori
+
+    logging.debug("Supplied arguments: %s", args)
+    logging.debug("Arg weights as float: %s", arg_weights)
+
+    # Call pyvolcans
+    try:
+        # calculated_weighted_analogy_matrix
+        my_weighted_matrix = calculate_weighted_analogy_matrix(
+            weights=arg_weights)
+
+        # calling the get_analogies function to derive the final data
+        get_analogies(volcano_name, my_weighted_matrix, count)
+
+        # calling the get_many_analogy_percentiles function
+        # to print 'better analogues'
+        if my_apriori_volcanoes is not None:
+            get_many_analogy_percentiles(volcano_name,
+                                         my_apriori_volcanoes,
+                                         my_weighted_matrix)
+    except PyvolcansError as exc:
+        # Print error message and quit program on error
+        logging.error(exc.args[0])
+        sys.exit(1)
+
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("volcano_name",
-                        help="Set target volcano",
+    parser.add_argument("volcano",
+                        help="Set target volcano name or Smithsonian ID",
                         type=str)
     parser.add_argument("--apriori", nargs='*',
                         help="Provide one or more a priori analogue volcanoes",
@@ -92,43 +122,8 @@ def cli():
 
     #'parsing the arguments'
     args = parser.parse_args()
-    if args.verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
-
-    #defining some intermediate variables
-    volcano_name = args.volcano_name
-    count = args.count
-    arg_weights = {'tectonic_setting': _frac_to_float(args.tectonic_setting),
-               'geochemistry': _frac_to_float(args.rock_geochemistry),
-               'morphology': _frac_to_float(args.morphology),
-               'eruption_size': _frac_to_float(args.eruption_size),
-               'eruption_style': _frac_to_float(args.eruption_style)}
-
-    my_apriori_volcanoes = args.apriori
-
-    logging.debug("Supplied arguments: %s", args)
-    logging.debug("Arg weights as float: %s", arg_weights)
-
-    # Call pyvolcans
-    try:
-        # calculated_weighted_analogy_matrix
-        my_weighted_matrix = calculate_weighted_analogy_matrix(
-            weights=arg_weights)
-
-        # calling the get_analogies function to derive the final data
-        get_analogies(args.volcano_name, my_weighted_matrix, count)
-
-        # calling the get_many_analogy_percentiles function
-        # to print 'better analogues'
-        if my_apriori_volcanoes is not None:
-            get_many_analogy_percentiles(args.volcano_name,
-                                         my_apriori_volcanoes,
-                                         my_weighted_matrix)
-    except PyvolcansError as exc:
-        # Print error message and quit program on error
-        logging.error(exc.args[0])
-        sys.exit(1)
-
+    
+    return args
 
 if __name__ == '__main__':
     cli()
