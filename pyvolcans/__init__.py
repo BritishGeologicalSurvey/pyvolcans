ae94235a7e428a399a437e29eebb6ec6c9d0fbb9
PTierz <pablo@bgs.ac.uk>
2021-03-26 15:31:05 +0000

    Update description of the PyVOLCANS package in __init.py__

--- pyvolcans/__init__.py
+++ pyvolcans/__init__.py
@@ -1,6 +1,35 @@
 """
-GENERAL DESCRIPTION FOR THE PACKAGE!! WHAT VOLCANS IS, A BIT OF WHY IT WAS
-CREATED.
+    An open-access Python tool that generates data-driven sets of analogue
+    volcanoes for any Holocene volcano listed in the Global Volcanism Program
+    (GVP) Volcanoes Of The World database (v. 4.6.7), based on the VOLCANS
+    (VOLCano ANalogues Search) method presented by Tierz, Loughlin and Calder
+    (2019): https://doi.org/10.1007/s00445-019-1336-3
+
+    VOLCANS uses five volcanological criteria (tectonic setting, rock
+    geochemistry, volcano morphology, eruption size and eruption style), and
+    a structured combination of them, to quantify overall multi-criteria (or
+    total) volcano analogy among any two volcanoes in the GVP database.
+    The data used by the method are extracted from the GVP database as well as
+    from a merged database of volcano morphology (after Pike and Clow, 1981;
+    Grosse et al., 2014).
+
+    PyVOLCANS provides its user with full flexibility to identify customised
+    sets of analogue volcanoes, by exploring three main variables:
+
+        (1) target volcano (or volcano of interest);
+        (2) weighting scheme (i.e. set of weights given to each of the five
+        volcanological criteria to calculate multi-criteria, total analogy);
+        (3) number of 'top' analogue volcanoes (i.e. those with the highest
+        value of analogy with the target volcano).
+
+    In addition, PyVOLCANS allows the user to compare the values of total
+    analogy computed for 'a priori analogues' (i.e. volcanoes thought to be
+    good analogues to the target volcano by other strands of evidence, e.g.
+    expert knowledge) with those computed for the rest of volcanoes in the GVP
+    database. This permits investigation of sets of analogue volcanoes for
+    varied purposes, and makes PyVOLCANS a valuable complementary method to
+    expert-derived analogue volcanoes. Please see Tierz et al. (2019) for more
+    details on the VOLCANS method (https://doi.org/10.1007/s00445-019-1336-3).
 """
 
 from ._version import get_versions
